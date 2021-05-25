d0d8a6530ba53daffadc085cd602f121cb42bbae
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-05-25 12:52:14 +1000

    initial commit

---
+++ lib/find_deals.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "find_deals/version"
+
+module FindDeals
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/find_deals/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module FindDeals
+  VERSION = "0.1.0"
+end
