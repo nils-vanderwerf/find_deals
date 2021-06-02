730811399c5b95a6c9e03fe730800f68a7743ffd
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-06-02 19:20:25 +1000

    Fixed broken delete method, a lot of the broken loops

--- lib/find_deals/cli.rb
+++ lib/find_deals/cli.rb
@@ -1,3 +1,4 @@
+
 class FindDeals::CLI
     attr_accessor :city_input, :category_input, :input, :user_name, :selected_deal
 
@@ -15,20 +16,22 @@ class FindDeals::CLI
         puts ""
         puts "Welcome to this amazing promo finder!"
         puts ""
-        # while @input != "quit"
+        start
+    end
+
+    def start
             #-----LIST OF METHODS---#
-        if @input != "quit"
             prompt_user_city
             prompt_user_category
-            print_deals
-            print_more_info
-            prompt_to_save_deal
-            prompt_to_show_saved_deals
-            next_steps
-        # end
-        else 
-            goodbye #quit program
-        end
+            if FindDeals::Deal.all.length != 0 
+                print_deals
+                print_more_info
+                prompt_to_save_deal
+                prompt_to_show_saved_deals
+                next_steps
+            else 
+                start
+            end
     end
 
     def prompt_user_city
@@ -51,29 +54,24 @@ class FindDeals::CLI
 
     def get_city_input
         @input = gets.strip
-        @input != 'quit' ? @city_input = select_city_from_input(@input) : goodbye
+        @input != 'quit' ? @city_input = select_city_from_input : goodbye
     end
 
-    def select_city_from_input(input)
-        while input.to_i == 0 || input.to_i > Cities.all.size
-            input = ""
+    def select_city_from_input
+        while @input.to_i == 0 || @input.to_i > Cities.all.size
             invalid_input
             prompt_user_city
-            @input = gets.strip
-            
         end
-            Cities.find(input).name ## Collects it from the databasekm 
+            Cities.find(@input).name ## Collects it from the database 
     end
 
-    def select_category_from_input(input)
-        while input.to_i == 0 || input.to_i > Categories.all.size
+    def select_category_from_input
+        while @input.to_i == 0 || @input.to_i > Categories.all.size
             invalid_input
             prompt_user_category
         end
-            Categories.find(input).name
+            Categories.find(@input).name ## Collects it from the database 
     end
-
-  
 
     def prompt_user_category
         puts "----------------------------------------------------------------"
@@ -96,26 +94,28 @@ class FindDeals::CLI
 
     def get_category_input
         @input = gets.strip
-        # @category_input = select_category_from_input(input)
+        @category_input = select_category_from_input
+        if @input == 'quit'
+            goodbye
+        end  
+        
         if @input.to_i == 0 || @input.to_i > Categories.all.length
             invalid_input
             get_category_input
-        elsif @input == 'quit'
-            goodbye
-        end  
-        elsif FindDeals::Deal.all.length == 0
+        end
+        @category_input = Categories.find(input).name
+        site_scraper = FindDeals::Scraper.new(@city_input, @category_input)
+        if FindDeals::Deal.all.length == 0
             puts "--------------------------------------------------------------------"
             puts ""
             puts "Sorry! No deals for this selection today. Please try another selection."
             puts ""
             puts "--------------------------------------------------------------------"
         end
-        @category_input = Categories.find(input).name
-        site_scraper = FindDeals::Scraper.new(@city_input, @category_input)
     end
 
     def print_deals
-        puts "----------------------------------------------------------------"
+        puts "--------------------------------------------------------------------"
         puts ""
         puts "DEALS FOR THIS INPUT"
         puts ""
@@ -125,59 +125,68 @@ class FindDeals::CLI
         end
     end
     
-        def print_more_info
-            
-            if FindDeals::Deal.all.length != 0
-                puts "--------------------------------------------------------------------"
-                puts ""
-                puts "Please enter the number of the deal you'd like to see more about. Type in quit to exit"
-                puts ""
-                @input = gets.strip.to_i
-                while @input == 0 || @input > FindDeals::Deal.all.size
-                    invalid_input
-                    @input = gets.strip.to_i
-                end
-                FindDeals::Deal.all[@input - 1].print_about_details
-                @selected_deal = FindDeals::Deal.all[@input - 1]
-            else
-                puts "--------------------------------------------------------------------"
-                puts ""
-                puts "Sorry! No deals for this selection today. Please try another selection."
-                puts ""
-                puts "--------------------------------------------------------------------"
-            end
-        end
-
-        def prompt_to_save_deal
+    def print_more_info
+        if FindDeals::Deal.all.length != 0
+            puts "--------------------------------------------------------------------"
             puts ""
-            puts "Would you like to save this deal? Y or N"
-            @input = gets.strip.downcase
+            puts "Please enter the number of the deal you'd like to see more about. Type in quit to exit"
+            puts ""
+            @input = gets.strip.to_i
+            while @input == 0 || @input > FindDeals::Deal.all.size
+                invalid_input
+                @input = gets.strip.to_i
+            end
+            FindDeals::Deal.all[@input - 1].print_about_details
+            @selected_deal = FindDeals::Deal.all[@input - 1]
+        else
+            puts "--------------------------------------------------------------------"
+            puts ""
+            puts "Sorry! No deals for this selection today. Please try another selection."
             puts ""
             puts "--------------------------------------------------------------------"
-            if @input == "y"
-                user = get_user
-                puts "Saving deal..."
-                puts ""
-                puts "--------------------------------------------------------------------"
-                
-                @selected_deal.save(user.id) ##save to Saved Deals, with the user id as the User_id
-            elsif @input == "n"
-                
-            else
-                invalid_input
-                prompt_to_save_deal
-            end
         end
+        
+        if @input == 'quit'
+            goodbye
+        end
+    end
+
+    def prompt_to_save_deal
+        puts ""
+        puts "Would you like to save this deal? Y or N"
+        @input = gets.strip.downcase
+        puts ""
+        puts "--------------------------------------------------------------------"
+        if @input == "y"
+            user = get_user
+            puts "Saving deal..."
+            puts ""
+            puts "--------------------------------------------------------------------"
+            
+            @selected_deal.save(user.id) ##save to Saved Deals, with the user id as the User_id
+        elsif @input == "n"
+            next_steps
+        elsif @input == 'quit'
+            goodbye   
+        else
+            invalid_input
+            prompt_to_save_deal
+        end
+    end
 
     def get_user
         puts "--------------------------------------------------------------------"
         puts ""
         puts "We need to be able to associate this deal with you."
         puts "Please enter a username"
-        @user_name = gets.strip
-        puts ""
-        puts "--------------------------------------------------------------------"
-        Users.find_or_create_by(name: @user_name)
+        @input = gets.strip
+        if @input == 'quit'
+            goodbye
+        else 
+            puts "--------------------------------------------------------------------"
+            @user_name = input
+            Users.find_or_create_by(name: @user_name)
+        end
     end
     
     def prompt_to_show_saved_deals
@@ -192,6 +201,8 @@ class FindDeals::CLI
             show_saved_deals
         elsif @input == "n"
             next_steps
+        elsif @input == 'quit'
+            goodbye
         else 
             invalid_input
             prompt_to_show_saved_deals
@@ -199,7 +210,6 @@ class FindDeals::CLI
 
     end
 
-    
     def show_saved_deals
         puts ""
         puts "--------------------------------------------------------------------"
@@ -264,55 +274,64 @@ class FindDeals::CLI
         puts "- To view more deals, type 'MORE'"
         puts "- To filter your saved deals by city, type in 'CITY'"
         puts "- To filter your saved deals by category, type in 'CATEGORY'"
-        puts "- To delete a saved deal type 'DELETE"
+        puts "- To delete a saved deal type 'DELETE'"
         puts "- To quit, type in quit"
         @input = gets.strip.downcase
 
-            if @input == "more"
-                puts ""
-                puts "--------------------------------------------------------------------"
-                # @city_input = ""
-                # @category_input = ""
-                prompt_user_city
-            elsif @input == 'city'
-                puts ""
-                puts "--------------------------------------------------------------------"
-                # @city_input = ""
-                # @category_input = ""
-                prompt_user_city
-                filter_by_city
-                next_steps
-            elsif @input == 'category'
-                prompt_user_category
-                filter_by_category
-                next_steps
-            elsif @input == "quit"
-                goodbye
-            elsif @input == "delete"
-                delete_record
-                next_steps
-            else  
-                invalid_input
-                next_steps
-            end
+        if @input == "more"
+            puts ""
+            puts "--------------------------------------------------------------------"
+            # start program again
+            start #
+        elsif @input == 'city'
+            puts ""
+            puts "--------------------------------------------------------------------"
+            prompt_user_city
+            filter_by_city
+            next_steps
+        elsif @input == 'category'
+            prompt_user_category
+            filter_by_category
+            next_steps
+        elsif @input == "delete"
+            delete_record
+            next_steps
+        elsif @input == "quit"
+            goodbye
+        else  
+            invalid_input
+            next_steps
+        end
     end
 
     def delete_record 
+        user = get_user
+        show_saved_deals
         puts "--------------------------------------------------------------------"
         puts ""
         puts "Type in the number of the deal you would like to delete"
-        @input = gets.strip.downcase
         puts ""
         puts "--------------------------------------------------------------------" 
-        
-        if @input.to_i != 0 && @input.to_i <= SavedDeals.all.size
-            SavedDeals.all.delete_from_db(input.to_i)
-            prompt_to_show_saved_deals
-            next_steps
-        else 
-           invalid_input
-           delete_record
+        @input = gets.strip.downcase
+
+        while @input.to_i == 0 || @input.to_i >= SavedDeals.select {|deal| deal.user_id == user.id}.length
+            invalid_input
+            puts "--------------------------------------------------------------------"
+            puts ""
+            puts "Type in the number of the deal you would like to delete"
+            puts ""
+            puts "--------------------------------------------------------------------" 
+            @input = gets.strip.downcase
+            
+            if @input == 'quit'
+                goodbye
+            end
         end 
+
+        SavedDeals.all.delete_from_db(@input.to_i)
+        prompt_to_show_saved_deals
+        next_steps
+
     end
 
     def invalid_input
